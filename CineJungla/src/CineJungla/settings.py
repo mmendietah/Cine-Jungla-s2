080df2dee7b8611d759bc171e1f006f5a645c7d2
MiguelMh <mmendietah@unbosque.edu.co>
2020-04-06 15:26:48 -0500

    q

--- CineJungla/src/CineJungla/settings.py
+++ CineJungla/src/CineJungla/settings.py
@@ -80,13 +80,11 @@ WSGI_APPLICATION = 'CineJungla.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
 }
 # DATABASES = {
 #     'default': {
