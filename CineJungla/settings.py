9ad11e42afb8dab18ee277b70f3545647e6be784
MiguelMh <mmendietah@unbosque.edu.co>
2020-04-06 18:15:18 -0500

    static problem

--- CineJungla/settings.py
+++ CineJungla/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_pro", "static"),
+    os.path.join(BASE_DIR, "static"),
     # '/var/www/static/'
 ]
 
